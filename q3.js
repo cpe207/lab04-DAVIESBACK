c7b852b9f89227df1e920b2a49a69d6ed37796ed
win <wwinnn478@gmail.com>
2024-07-01 23:48:57 +0700

    Lab04

--- q3.js
+++ q3.js
@@ -2,16 +2,16 @@ function primeNumber(a) {
   if (a <= 1) {
     return "No";
   }
-  for (let i = 2; i <= a / 2; i++) {
+  for (let i = 2; i <= Math.sqrt(a); i++) {
     if (a % i === 0) return "No";
   }
   return "Yes";
 }
 
 // Test cases
-const d1 = 10; 
-const d2 = 29; 
-const d3 = 2;  
+const d1 = 10;
+const d2 = 29;
+const d3 = 2;
 
 console.log(primeNumber(d1)); 
 console.log(primeNumber(d2)); 
